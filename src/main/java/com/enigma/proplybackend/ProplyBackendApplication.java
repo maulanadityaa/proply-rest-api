0bc2fb60af3b4ad03f2a9ec3f90c9d60421d2484
maulanadityaa <maulanadityaaa@gmail.com>
2024-05-18 10:06:40 +0700

    Initial commit : proply-backend

---
+++ src/main/java/com/enigma/proplybackend/ProplyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.proplybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProplyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProplyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enigma/proplybackend/ProplyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.proplybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProplyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
